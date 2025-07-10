c8ec6245502cbfc345a782bebbbdb8bf297c7ad8
Gean Luca <gean.luca@ingacode.com.br>
2025-07-09 23:37:58 -0300

    -feat adiciona alterações

---
+++ IngaTasks/Api-IngaTasks/Api-IngaTasks.Application/Validators/Collaborator/CollaboratorEditDtoValidator.cs
@@ -0,0 +1,21 @@
+﻿using Api_IngaTasks.Application.Dtos.Collaborator;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api_IngaTasks.Application.Validators.Collaborator
+{
+
+    public class CollaboratorEditDtoValidator : AbstractValidator<CollaboratorEditDto>
+    {
+        public CollaboratorEditDtoValidator()
+        {
+            RuleFor(x => x.Id)
+                .NotNull()
+                .WithMessage("Não pode ser vazio.");
+        }
+    }
+}
---
+++ IngaTasks/Api-IngaTasks/Api-IngaTasks.Application/Validators/User/UserUpdateDtoValidator.cs
@@ -0,0 +1,20 @@
+﻿using Api_IngaTasks.Application.Dtos.User;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api_IngaTasks.Application.Validators.User
+{
+    public class UserUpdateDtoValidator : AbstractValidator<UserUpdateDto>
+    {
+        public UserUpdateDtoValidator() 
+        {
+            RuleFor(x => x.Id)
+                .NotEmpty()
+                .WithMessage("O id é obrigatório.");
+        }
+    }
+}
